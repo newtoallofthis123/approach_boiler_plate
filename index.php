3cec58b22f56ab1c83322a9fcd02ed75e7e7a36a
newtoallofthis123 <noobscience123@gmail.com>
2024-03-26 18:26:33 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Template;
+require_once __DIR__ . '/support/lib/vendor/autoload.php';
+
+use Approach\Render\HTML;
+
+$x = new HTML(tag: "div", content: "Hello World");
+
+echo $x->render();
